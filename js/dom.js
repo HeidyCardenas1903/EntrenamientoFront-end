519331f1ced6cac3cb7f9d42a998b6852e205f03
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2023-09-07 21:09:05 -0500

    first commit

---
+++ js/dom.js
@@ -0,0 +1,80 @@
+// const figura = document.getElementById("figura");
+// console.log(figura);
+
+function circulo() {
+    // const figura = document.getElementById("figura");
+    const figura = document.getElementById("figura");
+    const titulo = document.querySelector(".titulo");
+
+    figura.classList.toggle("circulo");
+    titulo.textContent='CIRCULO'
+}
+
+function hor() {
+    const figura = document.getElementById("figura");
+    const titulo = document.querySelector(".titulo");
+
+    figura.classList.toggle("hor")
+    titulo.textContent='HORIZONTAL'
+}
+
+function ver(){
+    const figura = document.getElementById("figura");
+    const titulo = document.querySelector(".titulo");
+
+    figura.classList.toggle("ver");
+    titulo.textContent='VERTICAL'
+}
+
+function gif(){
+    const figura= document.getElementById("figura")
+    const titulo = document.querySelector(".titulo");
+
+    figura.classList.toggle("gif");
+    titulo.textContent='GIF'
+}
+
+function degrade(){
+    const figura = document.getElementById("figura");
+    const titulo = document.querySelector(".titulo");
+    
+    figura.classList.toggle("degrade");
+    titulo.textContent='DEGRADADO'
+}
+
+function panelLateral(){
+    const panel = document.querySelector(".panel-lateral");
+    panel.classList.toggle('active');
+}
+
+function moveTop(){
+    const figura = document.querySelector("#figura");
+    const titulo = document.querySelector(".titulo");
+
+    figura.classList.toggle('top')
+    titulo.textContent='TOP'
+}
+
+function moveBottom(){
+    const figura = document.querySelector("#figura");
+    const titulo = document.querySelector(".titulo");
+
+    figura.classList.toggle('bottom')
+    titulo.textContent='BOTTOM'
+}
+
+function moveLeft(){
+    const figura = document.querySelector("#figura");
+    const titulo = document.querySelector(".titulo");
+    
+    figura.classList.toggle('left')
+    titulo.textContent='LEFT'
+}
+
+function moveRight(){
+    const figura = document.querySelector("#figura");
+    const titulo = document.querySelector(".titulo");
+    
+    figura.classList.toggle('right')
+    titulo.textContent='RIGHT'
+}
